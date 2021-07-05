e50f668f16a5ffb0d0f20c9ce3780f47d61a4c7b
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2021-07-05 16:17:41 -0500

    super ninja

--- OOP/superNinja.js
+++ OOP/superNinja.js
@@ -18,5 +18,28 @@ class Ninja {
         this.health += 10;
     }
 
+}
+
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name, 200, 10, 10);
+        this.wisdom = 10;
+    }
+
+    speakWisdom() {
+        console.log("What one programmer can do in one month, two programmers can do in two months.");
+    }
+
+    showStats() {
+        return super.showStats();
+    }
+
+    drinkSake() {
+        console.log(super.drinkSake());
+    }
+
 
 }
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
